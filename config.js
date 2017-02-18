0958b5283667570ad9afecaedc1979ef6cb3e5bc
wednesday_developer <wednesday.developer@gmail.com>
2017-02-18 00:31:38 +0000

    congfig を分離

---
+++ config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyAveHsGsftBhoEqiqhqR5CpaEXDLztqMEE",
+	authDomain: "myfirst-a1b68.firebaseapp.com",
+	databaseURL: "https://myfirst-a1b68.firebaseio.com",
+	storageBucket: "",
+	messagingSenderId: "49152447799"
+};
+firebase.initializeApp(config);
