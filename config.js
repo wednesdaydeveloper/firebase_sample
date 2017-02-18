80aa5002de8aa18ae15321336e770dfd9ca66221
wednesday_developer <wednesday.developer@gmail.com>
2017-02-18 00:48:52 +0000

    congfig を削除

--- config.js
+++
@@ -1,9 +0,0 @@
-// Initialize Firebase
-var config = {
-	apiKey: "AIzaSyAveHsGsftBhoEqiqhqR5CpaEXDLztqMEE",
-	authDomain: "myfirst-a1b68.firebaseapp.com",
-	databaseURL: "https://myfirst-a1b68.firebaseio.com",
-	storageBucket: "",
-	messagingSenderId: "49152447799"
-};
-firebase.initializeApp(config);
